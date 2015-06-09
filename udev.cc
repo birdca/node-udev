75d981fb63fb690a09f952ef9e2b1bf55f1b2b0b
Ca Chen <kiancaca@gmail.com>
2015-06-09 12:10:15 +0800

    rearrange code format

--- udev.cc
+++ udev.cc
@@ -137,13 +137,16 @@ class Monitor : public node::ObjectWrap
 NAN_METHOD(List)
 {
     NanScope();
+
     Local<Array> list = NanNew<Array>();
-    struct udev_enumerate* enumerate;
-    struct udev_list_entry* devices;
-    struct udev_list_entry* entry;
-    struct udev_device *dev;
+
+    struct udev_enumerate  *enumerate;
+    struct udev_list_entry *devices;
+    struct udev_list_entry *entry;
+    struct udev_device     *dev;
 
     enumerate = udev_enumerate_new(udev);
+
     // add match etc. stuff.
     udev_enumerate_scan_devices(enumerate);
     devices = udev_enumerate_get_list_entry(enumerate);
@@ -153,7 +156,7 @@ NAN_METHOD(List)
     {
         const char *path;
         path = udev_list_entry_get_name(entry);
-        dev = udev_device_new_from_syspath(udev, path);
+        dev  = udev_device_new_from_syspath(udev, path);
         Local<Object> obj = NanNew<Object>();
         PushProperties(obj, dev);
         obj->Set(NanNew("syspath"), NanNew(path));
@@ -168,9 +171,11 @@ NAN_METHOD(List)
 NAN_METHOD(GetMountPoint)
 {
     NanScope();
+
     v8::String::Utf8Value pathObj(args[0]->ToString());
+
     struct mntent   *mnt;
-    FILE            *fp = NULL;
+    FILE            *fp   = NULL;
     const char      *path = *pathObj;
 
     // TODO: find a better way to replace waiting for a second
@@ -199,6 +204,7 @@ static void Init(Handle<Object> target)
     {
         NanThrowError("Can't create udev\n");
     }
+
     target->Set(NanNew("list"), NanNew<FunctionTemplate>(List)->GetFunction());
     target->Set(NanNew("getMountPoint"), NanNew<FunctionTemplate>(GetMountPoint)->GetFunction());
 
